48c5af4321d84da9f9203e44921113f28d1d01a2
DYLAN SNEIDER SANCHEZ ROJAS <62730083+Idiem@users.noreply.github.com>
2023-11-19 18:58:21 -0500

    add icon to page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "@/utils/globals.css";
-import Providers from "@/redux/providers";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import '@/utils/globals.css';
+import Providers from '@/redux/providers';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Crypto Tracker",
-  description: "A simple crypto tracker built with Next.js",
+  title: 'Crypto Tracker',
+  description: 'A simple crypto tracker built with Next.js',
 };
 
 export default function RootLayout({
@@ -18,6 +18,12 @@ export default function RootLayout({
   return (
     <Providers>
       <html lang="en">
+        <head>
+          <link rel="icon" href="/currency.ico" />
+          <meta name="description" content="A simple crypto tracker built with Next.js" />
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width" />
+        </head>
         <body className={inter.className}>{children}</body>
       </html>
     </Providers>
